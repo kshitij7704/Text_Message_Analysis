006b8b8eb31de01e2ce250f329dfdcc6d243bd33
kshitij7704 <kashyapkshitij7704@gmail.com>
2025-03-07 20:02:36 +0530

    Added streamlit file

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import preprocess
+
+st.sidebar.title("Whatsapp Chat Analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    df = preprocess.preprocess(data)
+    st.dataframe(df)
